0ba7c3c078ecd4ca72a84a50c551823309e97337
Andrey Potyomkin <and_tem@icloud.com>
2021-11-13 18:53:33 +0300

    use-fonts. fix deps example. updated config

--- examples/react-config/webpack.config.js
+++ examples/react-config/webpack.config.js
@@ -1,16 +1,39 @@
 const path = require('path');
 
-const { createConfig, compose } = require('@webpackon/react-config');
+const { compose } = require('@webpackon/core');
 const { useTs } = require('@webpackon/use-typescript');
-const { useReactRefresh } = require('@webpackon/use-react-refresh');
+const { useReact } = require('@webpackon/use-react');
+const { useHtmlTemplate } = require('@webpackon/use-html');
+const { useBabel } = require('@webpackon/use-babel');
+const { useCss } = require('@webpackon/use-css');
+const { useFonts } = require('@webpackon/use-fonts');
+const { useImages } = require('@webpackon/use-images');
+const { useOptimization } = require('@webpackon/use-optimization');
 
-module.exports = createConfig({
-  templatePath: path.resolve(__dirname, 'public', 'index.html'),
-  useTs: true,
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
-  modify: (config, { mode }) =>
-    compose(
-      useReactRefresh({ mode }),
-      useTs({ transpileLoaderUseItems: ['babel-loader'] })
-    )(config),
-});
+// module.exports = createConfig({
+//   templatePath: path.resolve(__dirname, 'public', 'index.html'),
+//   useTs: true,
+//   entry: path.resolve(__dirname, 'src', 'index.tsx'),
+//   modify: (config, { mode }) =>
+//     compose(
+//       useReactRefresh({ mode }),
+//       useTs({ transpileLoaderUseItems: ['babel-loader'] })
+//     )(config),
+// });
+
+module.exports = (_, { mode }) =>
+  compose(
+    useReact({ mode }),
+    useHtmlTemplate({
+      templatePath: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    useBabel(),
+    useTs({ transpileLoaderUseItems: ['babel-loader'] }),
+    useCss({ mode }),
+    useFonts(),
+    useImages({ mode }),
+    useOptimization({ mode })
+  )({
+    target: 'web',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  });
